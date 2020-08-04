8e59583bd8f6051eccfa913d15240dcc695fd21f
takayasumorishita <c011729632@edu.teu.ac.jp>
2020-08-05 04:32:03 +0900

    Fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,12 +5,16 @@ export default {
    ** Nuxt rendering mode
    ** See https://nuxtjs.org/api/configuration-mode
    */
-  mode: 'universal',
+  mode: 'spa',
   /*
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
   target: 'server',
+  server: {
+    port: 443, // デフォルト: 3000
+    host: '0.0.0.0', // デフォルト: localhost
+  },
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
